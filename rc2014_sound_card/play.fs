a4db7f9a339a931365612dd88c999bb18d0c1707
George Beckett <markgbeckett@gmail.com>
2021-01-19 20:05:31 +0000

    Added Forth wrapper for PLAY command

--- rc2014_sound_card/play.fs
+++ rc2014_sound_card/play.fs
@@ -1,62 +1,31 @@
-216 CONSTANT REG
-208 CONSTANT DAT
-
-CREATE SNDNOTES ( C, C#, D, D#, E, F, F#, G, G#, A, A#, B )
-438 , 404 , 381 , 360 , 339 , 320 , 302 , 285 , 269 , 254 , 240 , 226
-
-DEFINER CODE DOES> CALL ;
-
-CODE HALT 118 C, 253 C, 233 C,
-    
-: PAUSE ( N -- )
-    0
-    DO
-	HALT
-    LOOP
+: GETADDR
+ 2 * -4000 + @
+ 1+
 ;
 
-: GETNOTE
-    ( N -- NOTE )
-    2 * SNDNOTES +
-    @
-;
-: SNDSETUP ( VAL -- )
-    7 REG OUT
-    DAT OUT
+: COUNT
+ DUP 1+ SWAP C@
 ;
 
-: SETVOL ( VOL CHANNEL -- )
-    7 + REG OUT
-    DAT OUT
-;
-
-: SNDMUTE ( CHAN -- )
-    0 SWAP
-    SETVOL
-;
-
-: SNDMUTEALL
-    4 1 DO
-	I SNDMUTE
-    LOOP
-;
-
-: PLAYNOTE ( NOTE CHANNEL -- )
-    1- 2 *
-    DUP REG OUT
-    OVER
-    256 MOD DAT OUT
-    1+ REG OUT
-    256 / DAT OUT
-;
-
-: TEST
-    -1 11 9 8 6 3 2 0
-    BEGIN
-	DUP 1+
-    WHILE
-	    GETNOTE
-	    1 PLAYNOTE
-	    20 PAUSE
-    REPEAT
+DEFINER PLAY
+ 0
+ BEGIN
+  20 WORD COUNT DUP C,
+  OVER + SWAP
+  DO
+   I C@ C,
+  LOOP
+  1+ DUP 3 =
+ UNTIL
+ DROP
+DOES>
+ 0
+ BEGIN
+  >R DUP 1+ SWAP C@
+  OVER OVER + >R OVER
+  + I' GETADDR 4 +
+  ! I' GETADDR ! R>
+  R> 1+ DUP 3 =
+ UNTIL
+ DROP DROP -3FFA CALL
 ;
