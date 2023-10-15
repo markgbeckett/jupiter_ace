da6cd836d3531f2578cd0817fc50a9cae6200758
George Beckett <markgbeckett@gmail.com>
2023-10-15 08:50:06 +0100

    Updated scroll logic for Ace version

--- z80_disassembler/z80_dis_ace.asm
+++ z80_disassembler/z80_dis_ace.asm
@@ -8,7 +8,7 @@
 DISS:		dw 0x2701		; Address of AceFORTH pad
 ADDRESS:	equ 0x27FE
 OLD_SP:		equ 0x27FC
-SCROLL_COUNT:	equ 0x27FB
+LINE_COUNT:	equ 0x27FB
 	
 
 EXT_ADDR: equ 0x01
@@ -20,7 +20,8 @@ REL_ADDR:	equ 0x05
 SCRPOS:	equ 0x3C1C
 L_HALF:	equ 0x3C24
 KEYSCAN: equ 0x0336
-SCROLL:	equ 0x0421
+SCROLL:	equ 0x0421		; Routine to scroll screen and move
+				; SCRPOS up by one line
 	
 	;; Initialise display, etc.
 INIT:
@@ -34,37 +35,93 @@ INIT:
 	res 3,(IX+0x3E)	
 
 	;; Reset scroll count
-	ld a, 0x16
-	ld (SCROLL_COUNT),a
+	xor a
+	ld (LINE_COUNT),a
 	
 	ret
 
 	;; Print A to screen, protecting registers
 PRINT_A:
+	;; Save current registers
 	push bc
 	push de
 	push hl
 
-	ld de,(SCRPOS)
-	ld hl,(L_HALF)
+	;; Check if need to scroll
+	ld de,(SCRPOS)		; Retrieve current print location
+	ld hl,(L_HALF)		; Retrieve start locn for lower screen
 	scf
 
-	sbc hl,de
-	ex de,hl
+	sbc hl,de		; Carry indicates if SCRPOS > L_HALF
+	ex de,hl		; HL = SCRPOS
 
-	push af
-	call c, CHECK_SCROLL
-	pop af
-	
+	jr nc, NO_SCROLL
+
+	call SCROLL
+
+	;; HL contains current print location
+
+NO_SCROLL:
+	;; Check for newline
 	cp 0x0D
-
 	jr z, NEWLINE
 
+	;; If not new line, then print character
 	ld (hl),a
 	inc hl
 
 	jr PRINT_CONT
 
+NEWLINE:
+	;; Update line count and check if need to pause
+	ld a,(LINE_COUNT)
+	inc a
+
+	cp 0x17
+
+	;; Continue if no need to pause
+	jr c, NO_WAIT
+	
+	push hl			; Save print posn
+
+	;; Wait for key press
+CHECK_LOOP:
+	call KEYSCAN
+	jr z, CHECK_LOOP
+
+	;; Check for Break
+	cp _SPACE
+	jr z, EXIT
+
+	;; Reset line count
+	xor a
+
+	;; Restore screen position
+	pop hl
+	
+NO_WAIT:
+	ld (LINE_COUNT),a
+
+	;;  Implement newline
+NEWLINE_ADVANCE:
+	inc hl
+	ld a,l
+	and 0x1F
+	jr nz, NEWLINE_ADVANCE
+
+PRINT_CONT:
+	;; Save new print position
+	ld (SCRPOS),hl
+
+	;; Restore registers
+	pop hl
+	pop de
+	pop bc
+
+	;; Done
+	ret
+
+	;; Advance to column 14
 TAB:	push hl
 	ld hl,(SCRPOS)
 
@@ -83,47 +140,6 @@ TAB_STEP:
 
 	ret
 	
-NEWLINE:
-	inc hl
-	ld a,l
-	and 0x1F
-	jr nz, NEWLINE
-
-PRINT_CONT:
-	ld (SCRPOS),hl
-
-	pop hl
-	pop de
-	pop bc
-
-	ret
-
-CHECK_SCROLL:
-	ld a,(SCROLL_COUNT)
-	inc a
-	cp 0x17
-
-	jr c, CHECK_CONT
-
-CHECK_LOOP:
-	call KEYSCAN
-	jr z, CHECK_LOOP
-
-	cp _SPACE
-	jr nz, RESET_SCROLL
-
-	jr EXIT
-	
-RESET_SCROLL:
-	xor a
-	
-CHECK_CONT:
-	ld (SCROLL_COUNT),a
-	
-	call SCROLL
-
-	ret
-
 	;; Return to Forth
 EXIT:	ld ix, 0x3C00
 	ld sp, (OLD_SP)
